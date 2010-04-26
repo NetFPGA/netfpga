0edbe950db6033dc4db7f871ff7b8e1717ae3de7
Kumar Sanghvi <divinekumar@gmail.com>
2010-04-26 22:59:46 +0530

    Kernel: nf2_export.h converted to Linux Kernel Coding style and
    comments made kernel doc compliant
    
    Signed-off-by: Kumar Sanghvi <divinekumar@gmail.com>

--- lib/C/kernel/nf2_export.h
+++ lib/C/kernel/nf2_export.h
@@ -1,5 +1,5 @@
 /* ****************************************************************************
- * $Id: nf2_export.h 3546 2008-04-03 00:12:27Z derickso $
+ * nf2_export.h 3546 2008-04-03 00:12:27Z derickso
  *
  * Module: nf2_export.h
  * Project: NetFPGA 2 Linux Kernel Driver
