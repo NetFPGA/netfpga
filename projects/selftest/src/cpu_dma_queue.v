5ac90f58f394599ee9b7d90be29ff9a335cf2dd2
Glen Gibb <grg@stanford.edu>
2010-05-16 15:33:27 -0700

    selftest: update to use new nf2_dma module

--- projects/selftest/src/cpu_dma_queue.v
+++ projects/selftest/src/cpu_dma_queue.v
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-// $Id: cpu_dma_queue.v 5990 2010-03-10 22:14:12Z grg $
+// vim:set shiftwidth=3 softtabstop=3 expandtab:
 //
 // Module: cpu_dma_queue.v
 // Project: NF2.1
@@ -12,30 +12,34 @@
 //
 ///////////////////////////////////////////////////////////////////////////////
 
-  module cpu_dma_queue
-    #(parameter DATA_WIDTH = 32,
+module cpu_dma_queue
+   #(
+      parameter DATA_WIDTH = 32,
       parameter CTRL_WIDTH=DATA_WIDTH/8,
       parameter DMA_DATA_WIDTH = `CPCI_NF2_DATA_WIDTH,
       parameter DMA_CTRL_WIDTH = DMA_DATA_WIDTH/8
-      )
+   )
    (
-    // --- DMA rd rxfifo interface
-    output reg cpu_q_dma_pkt_avail,
+      // --- DMA rd rxfifo interface
+      output                               cpu_q_dma_pkt_avail,
+      output                               cpu_q_dma_rd_rdy,
 
-    input cpu_q_dma_rd,
-    output [DMA_DATA_WIDTH-1:0] cpu_q_dma_rd_data,
-    output [DMA_CTRL_WIDTH-1:0] cpu_q_dma_rd_ctrl,
+      input                                cpu_q_dma_rd,
+      output [DMA_DATA_WIDTH-1:0]          cpu_q_dma_rd_data,
+      output [DMA_CTRL_WIDTH-1:0]          cpu_q_dma_rd_ctrl,
 
-    // DMA wr txfifo interface
-    output reg cpu_q_dma_nearly_full,
+      // DMA wr txfifo interface
+      output                               cpu_q_dma_nearly_full,
+      output                               cpu_q_dma_can_wr_pkt,
 
-    input cpu_q_dma_wr,
-    input [DMA_DATA_WIDTH-1:0] cpu_q_dma_wr_data,
-    input [DMA_CTRL_WIDTH-1:0] cpu_q_dma_wr_ctrl,
+      input                                cpu_q_dma_wr,
+      input                                cpu_q_dma_wr_pkt_vld,
+      input [DMA_DATA_WIDTH-1:0]           cpu_q_dma_wr_data,
+      input [DMA_CTRL_WIDTH-1:0]           cpu_q_dma_wr_ctrl,
 
-    // --- Misc
-    input                                reset,
-    input                                clk
+      // --- Misc
+      input                                reset,
+      input                                clk
     );
 
    // -------- Internal parameters --------------
@@ -43,78 +47,120 @@
 
    // ------------- Wires/reg ------------------
 
-   wire                                 rx_fifo_rd_en;
+   wire                                rx_fifo_rd_en;
 
-    // wires from rx_fifo
-   wire [`CPCI_NF2_DATA_WIDTH*9/8-1:0]   rx_fifo_dout;
-   wire [8:0] 				rx_fifo_rd_data_count;
-   wire                                 rx_fifo_almost_full, rx_fifo_full;
-   wire 				rx_fifo_empty;
-   wire [`CPCI_NF2_DATA_WIDTH*9/8-1:0] 	rx_fifo_din;
+   // wires from rx_fifo
+   wire                                rx_fifo_full;
+   wire                                rx_fifo_almost_full;
+   wire                                rx_fifo_prog_full;
+   wire                                rx_fifo_empty;
 
+   wire                                pkt_stored;
+   wire                                pkt_removed;
+   wire                                first_word_removed;
+
+   reg [3:0]                           pkt_count;
+   reg                                 input_in_pkt;
+   reg                                 output_in_pkt;
 
 
    // ------------- Modules -------------------
 
    // pkt data and ctrl stored in rx_fifo are in little endian
-   async_fifo_512x36_progfull_500 rx_tx_fifo
-     (
-      //wr
-      .din(rx_fifo_din),
-      .wr_data_count(  ),
-      .wr_en(rx_fifo_wr_en),
-      .full( rx_fifo_full ),
-      .prog_full(rx_fifo_almost_full),
-      .wr_clk(clk),
+   cdq_rx_fifo_512x36 rx_fifo (
+      .din        ({cpu_q_dma_wr_ctrl, cpu_q_dma_wr_data}),
+      .wr_en      (rx_fifo_wr_en),
 
-      //rd
-      .dout(rx_fifo_dout),
-      .rd_data_count(rx_fifo_rd_data_count),
-      .rd_clk(clk),
-      .rd_en(rx_fifo_rd_en),
-      .empty(rx_fifo_empty),
+      .dout       ({cpu_q_dma_rd_ctrl, cpu_q_dma_rd_data}),
+      .rd_en      (rx_fifo_rd_en),
 
-      //misc
-      .rst(reset)
-      );
+      .full       (rx_fifo_full),
+      .almost_full(rx_fifo_almost_full),
+      .prog_full  (rx_fifo_prog_full),
+      .empty      (rx_fifo_empty),
+
+      .clk        (clk),
+      .rst        (reset)
+   );
 
    // -------------- Logic --------------------
 
-   /* monitor when pkts are read */
-
-   /* if a packet is ready to be sent to the user data
-    * path from the CPU, then pipe it out */
-   assign rx_fifo_din = {cpu_q_dma_wr_ctrl, cpu_q_dma_wr_data};
    assign rx_fifo_wr_en = (cpu_q_dma_wr) && (!rx_fifo_full);
    assign rx_fifo_rd_en = (cpu_q_dma_rd) && (!rx_fifo_empty);
-   assign {cpu_q_dma_rd_ctrl, cpu_q_dma_rd_data} = rx_fifo_dout;
+
+   assign cpu_q_dma_can_wr_pkt = !rx_fifo_prog_full;
+   assign cpu_q_dma_nearly_full = rx_fifo_almost_full;
+
+   assign cpu_q_dma_rd_rdy = !rx_fifo_empty;
+   assign cpu_q_dma_pkt_avail = pkt_count != 'h0;
+
+   assign pkt_stored = input_in_pkt && cpu_q_dma_wr && cpu_q_dma_wr_ctrl != 'h0;
+   assign pkt_removed = output_in_pkt && cpu_q_dma_rd && cpu_q_dma_rd_ctrl != 'h0;
+   assign first_word_removed = !output_in_pkt && cpu_q_dma_rd;
 
 
-   //the dma_engine module in CPCI doesn't support
-   //pause in the middle of transfering a packet from Spartan
-   //to Virtex. The cpu_q_dma_nearly_full is only checked
-   //before a packet is transferred from the CPCI. So
-   //when cpu_q_dma_nearly_full is deasserted, the cpu tx_fifo
-   //must be able to take in a maximum legal packet.
-   //In NetFPGA 2.1 platform, that's 2KB.
-   //after dma_engine module is able to support pause in the course
-   //of DMA tx, it's safe to drive cpu_q_dma_nearly_full by
-   //the tx_fifo_almost_full signal.
-
-   /* run the counters and mux between write and update */
+   // Count the number of packets in the queue
    always @(posedge clk) begin
-      if(reset) begin
-	 cpu_q_dma_pkt_avail <= 1'b 0;
-	 cpu_q_dma_nearly_full <= 1'b 0;
-
-      end // if (reset)
-
+      if (reset) begin
+         pkt_count <= 'h0;
+         input_in_pkt <= 0;
+         output_in_pkt <= 0;
+      end
       else begin
-	 cpu_q_dma_pkt_avail <= ~rx_fifo_empty;
-	 cpu_q_dma_nearly_full <= rx_fifo_almost_full;
+         case (input_in_pkt)
+            1'b0: begin
+               if (cpu_q_dma_wr && cpu_q_dma_wr_ctrl == 'h0)
+                  input_in_pkt <= 1;
+            end
 
-      end // else: !if(reset)
+            1'b1: begin
+               if (cpu_q_dma_wr && cpu_q_dma_wr_ctrl != 'h0)
+                  input_in_pkt <= 0;
+            end
+         endcase
 
-   end // always @ (posedge clk)
+         case (output_in_pkt)
+            1'b0: begin
+               if (cpu_q_dma_rd && cpu_q_dma_rd_ctrl == 'h0)
+                  output_in_pkt <= 1;
+            end
+
+            1'b1: begin
+               if (cpu_q_dma_rd && cpu_q_dma_rd_ctrl != 'h0)
+                  output_in_pkt <= 0;
+            end
+         endcase
+
+         case ({pkt_stored, first_word_removed})
+            2'b10: pkt_count <= pkt_count + 'h1;
+            2'b01: pkt_count <= pkt_count - 'h1;
+         endcase
+      end
+   end
+
+   // Verify that only one header word exists on each packet
+
+   // synthesis translate_off
+   reg seen_header_word;
+   always @(posedge clk) begin
+      if (reset)
+         seen_header_word <= 0;
+      else begin
+         if (!input_in_pkt && cpu_q_dma_wr) begin
+            if (cpu_q_dma_wr_ctrl != 'h0) begin
+               if (!seen_header_word)
+                  seen_header_word <= 1;
+               else begin
+                  $display($time, " ERROR: %m: Saw two header words on an incoming packet. Expecting only one.");
+                  $finish;
+               end
+            end
+         end
+         else if (input_in_pkt && cpu_q_dma_wr) begin
+            seen_header_word <= 1;
+         end
+      end
+   end
+   // synthesis translate_on
 
 endmodule // cpu_dma_queue
