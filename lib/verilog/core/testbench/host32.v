1ac5207d8f239a74d7bce3ceeddb5f1a0fac4808
Glen Gibb <grg@stanford.edu>
2010-08-02 17:04:40 -0700

    sim: handle dma in progress/interrupts
    Earlier commit inadvertently removed some code that caused the PCI
    testbench to pause while a DMA transaction was in progress. (This code
    serviced interrupts during that time.)
    
    Restored that code with some changes specific to the new synchronization
    architecture.

--- lib/verilog/core/testbench/host32.v
+++ lib/verilog/core/testbench/host32.v
@@ -163,6 +163,26 @@ module host32 (
                   exp_pkts[i] = pci_cmds[pci_ptr + 1 + i];
             delay = {pci_cmds[pci_ptr+1], pci_cmds[pci_ptr+2]};
 
+            // Wait appropriately if the command is a PCI transaction
+            if (pci_cmd != `PCI_BARRIER && pci_cmd != `PCI_DELAY) begin
+                // Service any interrupts
+                while (~INTR_A)
+                   service_interrupt;
+                while (dma_in_progress ||
+                   (pci_cmd == `PCI_DMA && !dma_can_wr_pkt[pci_addr - 1])) begin
+
+                   // Wait until either an interrupt occurs
+                   // or the there's no DMA transfer in progress and we can
+                   // proceed with the next command
+                   wait (~INTR_A || !dma_in_progress &&
+                      !(pci_cmd == `PCI_DMA && !dma_can_wr_pkt[pci_addr - 1]));
+
+                   // Service any interrupts
+                   if (~INTR_A)
+                      service_interrupt;
+                end
+             end
+
 	    // tell user what we're doing
 
             case (pci_cmd)
