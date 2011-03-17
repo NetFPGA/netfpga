c4148bfa3b1c254d0137d0359f199f560ab9afa9
Eric Lo <eric.lo.fh@gmail.com>
2011-03-16 17:31:09 -0700

    verif: Fix python hw sim libs for perf/stability
    Replace pktSend function with pktSend class which wraps a socket
    Implement interrupt based barrier
    Add pktCompare thread to do real time packet comparision in the background
    Optimize resolvePkts to increase efficiency
    Update TestLib wrapper with these changes
    Add helper methods len, generate_load to TestLib

--- lib/python/TestLib.py
+++ lib/python/TestLib.py
@@ -3,64 +3,236 @@
 import testPktHW
 import sys
 
+import time
+import random
+
+try:
+   import scapy.all as scapy
+except:
+   try:
+      import scapy as scapy
+   except:
+      sys.exit("Error: need to install scapy for packet handling")
+
 ifaceArray = []
 captureThreads = {}
-pktArray = {}
+openSockets = {}
+packets = {}
 toIgnore = {}
 
+############################
+# Function: nftest_init
+# Arguments: arguments passed to script
+#            list of interfaces to sniff
+# Description: handles options, populates ifaceArray
+############################
 def nftest_init(argv, active_ports):
     # process argv
     for iface in active_ports:
         ifaceArray.append(iface)
 
-def nftest_start(active_ports):
+############################
+# Function: nftest_start
+# Arguments: none
+# Description: starts capture threads
+############################
+def nftest_start():
     for iface in ifaceArray:
         captureThreads[iface] = testPktHW.pktExpect(iface)
-        pktArray[iface] = []
-        toIgnore[iface] = []
+        openSockets[iface] = testPktHW.pktSend(iface)
+        packets[iface] = {}
+        toIgnore['layer'] = []
+        toIgnore['method'] = []
         captureThreads[iface].start()
+        openSockets[iface].start()
     alive = False
+    # block until all capture threads are ready
     while not alive:
         alive = True
         for iface in ifaceArray:
             alive &= captureThreads[iface].isAlive()
 
+############################
+# Function: nftest_restart
+# Arguments: none
+# Description: resets all packet lists
+############################
 def nftest_restart():
-    for pkts in pktArray:
-        pkts = []
+    for pkts in packets:
+        pkts = {}
+    for iface in ifaceArray:
+        captureThreads[iface].restart()
 
+############################
+# Function: nftest_send
+# Arguments: interface name
+#            packet to send
+#            (optional) expect packet
+# Description: sends packet on an interface, expect the packet if expect is True
+############################
 def nftest_send(ifaceName, pkt, expect = True):
-    testPktHW.pktSend(ifaceName, pkt)
+    try:
+        openSockets[ifaceName].sendPkt(pkt)
+    except(KeyError):
+        print 'Error: invalid interface name'
     if expect:
         nftest_expect(ifaceName, pkt)
 
+############################
+# Function: nftest_expect
+# Arguments: interface name
+#            packet to expect
+# Description: expects a packet on an interface
+############################
 def nftest_expect(ifaceName, pkt):
     captureThreads[ifaceName].expectPkt(pkt)
 
-def nftest_barrier(timeout = 10, exit_on_failure = True):
-    good = True
-    for iface in ifaceArray:
-        good &= captureThreads[iface].barrier(timeout)
-    if exit_on_failure and not good:
-        nftest_finish()
-        sys.exit()
+############################
+# Function: nftest_barrier
+# Arguments: (optional) timeout in seconds
+# Description: blocks execution until all expected packets arrive, or times out
+#              returns False if timed out
+############################
+def nftest_barrier(timeout = 10):
+    start = time.clock()
+    good = False
+    while timeout + start - time.clock() > 0:
+        good = True
+        for iface in ifaceArray:
+            captureThreads[iface].compareEvent.set()
+        for iface in ifaceArray:
+            good &= captureThreads[iface].barrierEvent.wait(timeout-start)
+        if good:
+            break
+    if not good:
+        print 'Error: barrier timed out after', str(timeout), 'seconds'
+        for iface in ifaceArray:
+            numUnexp = captureThreads[iface].pkts.__len__()
+            numExp = captureThreads[iface].exp_pkts.__len__()
+            if numUnexp > 0:
+                print 'Error: device', iface, 'saw', str(numUnexp), 'unexpected packets'
+            if numExp > 0:
+                print 'Error: device', iface, 'missed', str(numExp), 'expected packets'
+        print ''
+    return good
 
+############################
+# Function: nftest_compare
+# Arguments: list of expected packets
+#            list of unexpected packets
+# Description: compares expected packets to unexpected packets, enumerating differences
+############################
+def nftest_compare(exp, unexp):
+    numExp = exp.__len__()
+    numUnexp = unexp.__len__()
+    # check if there is anything to compare
+    if numExp is 0 and numUnexp is 0:
+        return 0
+    elif numExp is 0:
+        print 'Error:', str(numUnexp), 'unexpected packets seen'
+        return numUnexp
+    elif numUnexp is 0:
+        print 'Error:', str(numExp), 'expected packets not seen'
+        return numExp
+
+    # print differences between exp and unexp pkts
+    print str(numExp), 'expected packets not seen'
+    print str(numUnexp), 'unexpected packets'
+    for i in range(numExp):
+        print 'Expected packet', str(i)
+        for j in range(numUnexp):
+            if len(exp[i]) is not len(unexp[j]):
+                print '   Unexpected packet ' + str(j) + ': Packet lengths do not match, expecting', str(len(exp[i])), 'but saw', str(len(unexp[j]))
+            else:
+                str_exp_pkt = ''
+                str_unexp_pkt = ''
+                # generate a hex string
+                for x in str(exp[i]):
+                    str_exp_pkt += "%02X"%ord(x)
+                for x in str(unexp[j]):
+                    str_unexp_pkt += "%02X"%ord(x)
+                for k in range(len(str_exp_pkt)):
+                    if str_unexp_pkt[k] is not str_exp_pkt[k]:
+                        print '   Unexpected packet ' + str(j) + ': byte', str(k/2), '(starting from 0) not equivalent (EXP:', str_exp_pkt[i:i+2] + ', ACTUAL:', str_unexp_pkt[k:k+2] + ')'
+                        break
+
+    return numExp + numUnexp
+
+############################
+# Function: nftest_finish
+# Arguments: none
+# Description: closes capture threads, filters by toIgnore, calls pktCmp, writes pcap files, return # errors
+############################
 def nftest_finish():
-    for iface in ifaceArray:
-        # close capture threads, record packets
-        pktArray[iface] = captureThreads[iface].finish()
-        # filter packets
-        try:
-            for layer in toIgnore['layer']:
-                pktArray[iface] = pktArray[iface].filter(lambda x: x.haslayer(layer))
-        except(KeyError):
-            pass
-        try:
-            for method in toIgnore['method']:
-                pktArray[iface] = pktArray[iface].filter(method)
-        except(KeyError):
-            pass
+    pkts = ()
+    ignored = []
+    error_count = 0
 
+    for iface in ifaceArray:
+        openSockets[iface].close()
+        # close capture threads, record packets
+        pkts = captureThreads[iface].finish()
+
+        # filter packets
+        packets[iface]['Matched'] = pkts[0]
+        packets[iface]['Unexpected'] = pkts[1]
+        packets[iface]['Expected'] = pkts[2]
+        nftest_filter(packets[iface]['Matched'], ignored)
+        nftest_filter(packets[iface]['Unexpected'], ignored)
+        nftest_filter(packets[iface]['Expected'], ignored)
+
+        # show differences between packets
+        error_count += nftest_compare(packets[iface]['Expected'], packets[iface]['Unexpected'])
+
+        # write pcap files - TO IMPLEMENT: write to tmp dir
+        if packets[iface]['Matched'].__len__() > 0:
+            scapy.wrpcap(iface+"_matched.pcap", packets[iface]['Matched'])
+        if packets[iface]['Expected'].__len__() > 0:
+            scapy.wrpcap(iface+"_expected.pcap", packets[iface]['Expected'])
+        if packets[iface]['Unexpected'].__len__() > 0:
+            scapy.wrpcap(iface+"_extra.pcap", packets[iface]['Unexpected'])
+        if ignored.__len__() > 0:
+            scapy.wrpcap(iface+"_ignored.pcap", ignored)
+    return error_count
+
+############################
+# Function: nftest_filter
+# Arguments: list of packets to filter, list to add ignored packets to
+# Description: removes packets to be ignored
+############################
+def nftest_filter(pktlist, ignorelist):
+    if pktlist.__len__() <= 0:
+        return
+    ignored = []
+    for layer in toIgnore['layer']:
+        for pkt in pktlist:
+            if pkt.haslayer(layer):
+                duplicate = False
+                for ignorepkt in ignored:
+                    if pkt is ignorepkt:
+                        duplicate = True
+                if not duplicate:
+                    ignored.append(pkt)
+    for method in toIgnore['method']:
+        for pkt in pktlist:
+            if method(pkt):
+                duplicate = False
+                for ignorepkt in ignored:
+                    if pkt is ignorepkt:
+                        duplicate = True
+                if not duplicate:
+                    ignored.append(pkt)
+    for pkt in ignored:
+        pktlist.remove(pkt)
+    ignorelist.extend(ignored)
+
+############################
+# Function: nftest_ignore
+# Arguments: filter
+#            (optional) method
+# Description: adds string name of scapy layer to ignore
+#              or adds method to ignore if method is True
+############################
 def nftest_ignore(filter, method = False):
     # filter by layer
     if not method:
@@ -68,3 +240,29 @@ def nftest_ignore(filter, method = False):
     # filter by method(pkt), returning truth value
     else:
         toIgnore['method'].append(filter)
+
+############################
+# Function: len
+# Arguments: packet
+# Description: overrides len(pkt) to account for packets of length < 60
+############################
+def len(pkt):
+    length = pkt.__str__().__len__()
+    if length < 60:
+        return 60
+    return length
+
+############################
+# Function: generate_load
+# Arguments: min_len
+#            (optional) max_len
+# Description: generates a random payload of min_len
+#              if max_len is specified, payload will be random length between min_len and max_len
+############################
+def generate_load(min_len, max_len = 0):
+    if max_len == 0:
+        max_len = min_len
+    load = ''
+    for i in range(random.randint(min_len,max_len)):
+        load += chr(random.randint(0,255))
+    return load
--- lib/python/testPktHW.py
+++ lib/python/testPktHW.py
@@ -1,11 +1,16 @@
-#1/usr/bin/env python
+#!/usr/bin/env python
 # Author: Eric Lo
-# Date: 2/9/2011
+# Date: 3/16/2011
 
 import sys
 from threading import Thread
 from threading import Lock
+from threading import Event
 import time
+import socket
+import os
+from collections import deque
+from select import select
 
 try:
    import scapy.all as scapy
@@ -16,7 +21,7 @@ except:
       sys.exit("Error: need to install scapy for packet handling")
 
 # Override sniff from scapy to implement custom stopper
-def sniff(count=0, store=1, offline=None, prn = None, lfilter=None, L2socket=None, timeout=None, stopperTimeout=None, stopper = None, *arg, **karg):
+def sniff(count=0, store=1, offline=None, prn = None, lfilter=None, L2socket=None, timeout=None, stopperTimeout=None, stopper = None, lst = [], *arg, **karg):
     """Sniff packets
 sniff([count=0,] [prn=None,] [store=1,] [offline=None,] [lfilter=None,] + L2ListenSocket args) -> list of packets
 
@@ -44,7 +49,7 @@ L2socket: use the provided L2socket
     else:
         s = PcapReader(offline)
 
-    lst = []
+    #lst = []
     if timeout is not None:
         stoptime = time.time()+timeout
     remain = None
@@ -94,8 +99,6 @@ L2socket: use the provided L2socket
     s.close()
     return scapy.plist.PacketList(lst,"Sniffed")
 
-
-
 class pktExpect(Thread):
     ############################
     # Function: __init__
@@ -105,41 +108,48 @@ class pktExpect(Thread):
     ############################
     def __init__ (self, device):
         Thread.__init__(self)
+        self.daemon = True
         self.device = device
-        self.pkts = []
         self.done = False
-        self.exp_pkts = []
-        self.unexp_pkts = []
-        self.missing_pkts = []
-        self.matched =[]
         self.ready = False
-        self.verbose = True # default should be false
+        self.count = 0
+        self.pkts = []
+        self.exp_pkts = []
+        self.matched =[]
         self.lock = Lock()
         self.status = -1
+        self.compareEvent = Event()
+        self.compare = pktCompare(self)
+        self.barrierEvent = Event()
 
     ############################
     # Function: run
     # Arguments: calling object
-    # Description: runs sniff
+    # Description: starts background comparison thread, runs sniff
     ############################
     def run(self):
+        self.compare.start()
         while not self.done:
             try:
                 sniff(prn=self.addPkt,iface=self.device, store=0, stopperTimeout=1, stopper=self.isDone)
             except(KeyboardInterrupt):
                 self.finish()
-        else:
-            sys.exit(0)
 
     ############################
     # Function: addPkt
     # Arguments: calling object
     #            packet to add to pkts
     # Description: adds packet to either matched or received packets list
+    #              notifies background thread to compare packet lists
     ############################
     def addPkt(self,pkt):
         self.pkts.append(pkt)
-        self.resolvePkts()
+        self.count += 1
+        self.barrierEvent.clear()
+        if self.compareEvent.is_set():
+            pass
+        else:
+            self.compareEvent.set()
 
     ############################
     # Function: isDone
@@ -163,29 +173,31 @@ class pktExpect(Thread):
     # Function: expectPkt
     # Arguments: calling object
     #            packet to expect
-    # Description: adds packet to list of packets to expect, rechecks packet lists
+    # Description: adds packet to list of packets to expect
     ############################
     def expectPkt(self, pkt):
         self.exp_pkts.append(pkt)
-        self.resolvePkts()
+        self.barrierEvent.clear()
 
     ############################
     # Function: resolvePkts
     # Arguments: calling object
     # Description: checks unmatched received packets against unmatched expected packets
+    #              notifies barrierEvent if expected and received packet lists are empty
     ############################
     def resolvePkts(self):
         self.lock.acquire()
-        self.pkts == None
-        self.exp_pkts == None
-        if (len(self.pkts) != 0) and  (len(self.exp_pkts) != 0):
+        if (len(self.pkts) > 0) and  (len(self.exp_pkts) > 0):
+            self.barrierEvent.clear()
             i = 0
             numPkts = len(self.pkts)
             while i < numPkts:
+                strpkt = str(self.pkts[i])
                 for exp in self.exp_pkts:
-                    matches = str(exp) == str(self.pkts[i])
-                    if not matches and self.pkts[i].haslayer(scapy.Padding):
-                        if str(exp/self.pkts[i][scapy.Padding]) == str(self.pkts[i]):
+                    strexp = str(exp)
+                    matches = (strexp == strpkt)
+                    if not matches and len(exp) < 60 and self.pkts[i].haslayer(scapy.Padding):
+                        if ''.join([strexp,str(self.pkts[i][scapy.Padding])]) == strpkt:
                             matches = True
                     if matches:
                         self.matched.append(self.pkts[i])
@@ -196,111 +208,127 @@ class pktExpect(Thread):
                         break
                 i += 1
         self.lock.release()
-        return True
-
-    ############################
-    # Function: barrier
-    # Arguments: calling object
-    #            timeout in seconds
-    # Description: pauses execution until expected packets arrive or timeout passes
-    ############################
-    def barrier(self, timeout):
-        self.resolvePkts()
-        start = time.clock()
-        # 10 sec timeout
-        while (len(self.exp_pkts) != 0) & ((time.clock() - start) < timeout):
-            pass
-        else:
-            if len(self.exp_pkts) == 0:
-                return True
-            else:
-                print '\nError: barrier timed out after ', str(timeout), ' seconds'
-                print 'Missing ', str(len(self.exp_pkts)), ' packet on device ', str(self.device), '\n'
+        if (len(self.pkts) is 0) and  (len(self.exp_pkts) is 0):
+            self.barrierEvent.set()
+            return True
         return False
 
     ############################
-    # Function: pktCmp
+    # Function: restart
     # Arguments: calling object
-    # Description: compares expected packets to received packets
-    #              prints differences and returns # of errors
+    # Description: resets all packet lists
     ############################
-    def pktCmp(self):
-        if len(self.pkts) == 0 and len(self.exp_pkts) == 0:
-            return 0
-        elif len(self.pkts) == 0:
-            return len(self.exp_pkts)
-        elif len(self.exp_pkts) == 0:
-            return len(self.pkts)
-        error = 0
-        exp_matched = []
-        # mark each egress packet as not matched
-        for pktNum in range(len(self.exp_pkts)):
-            exp_matched.append(0)
+    def restart(self):
+        self.lock.acquire()
+        self.exp_pkts = []
+        self.pkts = []
+        self.matched = []
+        self.lock.release()
 
-        # try to match expected pkt w/ egress pkt
-        for pktNum in range(len(self.pkts)):
-            pkt = self.pkts[pktNum] # pkt is packet to compare against
-            exp_index = 0 # will be index of matching exp_pkt
-            result = True
-            while ((exp_index < len(self.exp_pkts)) and result):
-                result = True # true - mismatch
-                if (exp_matched[exp_index] == 0):
-                    exp_pkt = self.exp_pkts[exp_index]
-                    result = (exp_pkt != pkt)
-                exp_index += 1
-
-            if result:
-                error += 1
-                str_exp_pkt = ''
-                str_pkt = ''
-                for x in str(exp_pkt):
-                    str_exp_pkt += "%02X"%ord(x)
-                for x in str(pkt):
-                    str_pkt += "%02X"%ord(x)
-                if self.verbose & (len(str_exp_pkt) != len(str_pkt)):
-                    print 'Packet lengths do not match, expecting ', str(len(exp_pkt)), ' but saw ', str(len(pkt))
-                else:
-                    for i in range(len(str_exp_pkt)):
-                        if self.verbose & (str_pkt[i] != str_exp_pkt[i]):
-                            print 'byte ', str(i/2), ' (starting from 0) not equivalent (EXP: ', str_exp_pkt[i:i+2], ', ACTUAL: ', str_pkt[i:i+2], ')'
-                            break
-            else:
-                exp_index -= 1
-        return error
 
     ############################
     # Function: finish
     # Arguments: calling object
-    # Description: calls pktCmp and prints results
-    #              tells sniff to stop sniffing
-    #              returns received packets
+    # Description: tells sniff to stop sniffing
+    #              returns packets
     ############################
     def finish(self):
         print self.device, 'finishing up'
-        #time.sleep(1) # make sure last few packets get in - DEPRECATED: barrier
-        #print self.pktCmp(), ' errors\n'
         self.done = True
-        #self.barrier(10)
-        print 'matched ', str(len(self.matched)), ' packets'
-        if len(self.exp_pkts) != 0:
-            if not (len(self.exp_pkts) is 1 and self.exp_pkts[0] is None):
-                print str(len(self.exp_pkts)), ' missing packets'
-                scapy.wrpcap(self.device+"_missing.pcap",self.exp_pkts)
-        if len(self.pkts) != 0:
-            if not (len(self.pkts) is 1 and self.pkts[0] is None):
-                print str(len(self.pkts)), ' extra packets'
-                scapy.wrpcap(self.device+"_extra.pcap",self.pkts)
-        print ''
-        self.pktCmp()
-        return self.pkts
+        self.lock.acquire()
+        return (self.matched, self.pkts, self.exp_pkts)
 
+class pktSend(Thread):
+    ############################
+    # Function: __init__
+    # Arguments: calling object
+    #            device name
+    #            socket configuration options
+    # Description: overrides Thread.__init__, initializes vars, opens socket
+    ############################
+    def __init__(self, device, family = socket.AF_PACKET, type = socket.SOCK_RAW, proto = socket.htons(3)):
+        Thread.__init__(self)
+        self.daemon = True
+        self.lock = Lock()
+        self.closed = False
+        self.toSend = deque()
+        self.sock = socket.socket(family, type, proto)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 0)
+        while True:
+            r,w,x = select([self.sock.fileno()], [], [], 0)
+            if r:
+                os.read(self.sock.fileno(),1600)
+            else:
+                break
+        self.sock.bind((device,3))
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, 2**30)
+        self.sentcount = 0
+        self.sendcount = 0
 
-############################
-# Function: pktSend
-# Arguments: calling object
-#            name of interface to sniff
-#            packet to send
-# Description: sends packet on an interface
-############################
-def pktSend(ifaceName, pkt):
-    scapy.sendp(pkt,iface=ifaceName)
+    ############################
+    # Function: run_
+    # Arguments: calling object
+    # Description: overrides Thread.run, sends packets in queue
+    ############################
+    def run(self):
+        while not self.closed:
+            try:
+                if len(self.toSend) > 0:
+                    self.lock.acquire()
+                    self.sock.send(self.toSend.popleft().__str__())
+                    self.sentcount += 1
+                    self.lock.release()
+            except(KeyboardInterrupt):
+                self.close()
+
+    ############################
+    # Function: sendPkt
+    # Arguments: calling object
+    #            packet to send
+    # Description: adds packets to queue to be sent
+    #              delays so scapy doesn't drop packets
+    ############################
+    def sendPkt(self, pkt):
+        self.lock.acquire()
+        self.toSend.append(pkt)
+        self.sendcount += 1
+        time.sleep(0.008)
+        self.lock.release()
+
+    ############################
+    # Function: close
+    # Arguments: calling object
+    # Description: waits for packets to drain, then closes socket
+    ############################
+    def close(self):
+        while len(self.toSend) > 0:
+            pass
+        if self.closed:
+            return
+        self.closed = True
+        self.sock.close()
+
+class pktCompare(Thread):
+    ############################
+    # Function: __init__
+    # Arguments: calling object
+    #            pktExpect object
+    # Description: overrides Thread.__init__, initializes vars
+    ############################
+    def __init__ (self, expect):
+        Thread.__init__(self)
+        self.daemon = True
+        self.done = False
+        self.compareEvent = expect.compareEvent
+        self.compare = expect.resolvePkts
+
+    ############################
+    # Function: run
+    # Arguments: calling object
+    # Description: waits until woken by addPkt, then calls resolvePkts
+    ############################
+    def run(self):
+        while not self.done:
+            self.compareEvent.wait()
+            self.compareEvent.clear()
+            self.compare()
