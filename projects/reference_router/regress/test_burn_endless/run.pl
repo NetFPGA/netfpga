1123b48cc54cec04bf03641f53810895e3469355
Eric Lo <eric.lo.fh@gmail.com>
2011-08-12 13:49:28 -0700

    Remove legacy tests for reference nic, router, switch
    Update regress.txt so nf_regress_test.pl doesn't try to run
    legacy tests

--- projects/reference_router/regress/test_burn_endless/run.pl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/perl -w
-
-system 'nice -n -19 ../src/burn.pl --filename \'>>/root/burn_log\' --noprint_to_console --num_ports 4 --run_length 2 --load_timeout 10.0 --len 256 --batch_size 1 --packets_to_loop 255';
-
--- projects/reference_router/regress/test_crash/run.pl
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/perl
-
-system 'nice -n -19 ./crash.pl --len 1496 --pkts 200';
