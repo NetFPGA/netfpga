d7c4d900297be49d8bf0978df9f88e8dd0ef1399
Jakub Kicinski <kubakici@wp.pl>
2014-05-15 17:50:02 +0200

    remove .phys_id from ethops
    .phys_id was removed in mainline Linux kernel in
    1ab7b6ac27 (ethtool: remove phys_id from ethtool_ops).

--- lib/C/kernel/nf2_ethtool.c
+++ lib/C/kernel/nf2_ethtool.c
@@ -86,17 +86,11 @@ static void nf2_get_drvinfo(struct net_device *dev,
 
 }
 
-static int nf2_phys_id(struct net_device *dev, __u32 data)
-{
-	return 0;
-}
-
 static const struct ethtool_ops nf2_ethtool_ops = {
 	.get_settings		= nf2_get_settings,
 	.set_settings		= nf2_set_settings,
 	.get_drvinfo		= nf2_get_drvinfo,
 	.get_link		= ethtool_op_get_link,
-	.phys_id		= nf2_phys_id,
 };
 
 void nf2_set_ethtool_ops(struct net_device *dev)
